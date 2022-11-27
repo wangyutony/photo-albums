bc7b4bfe423c2d032184a07880dbc8d36c1e9f1e
wangyutony <tonywangyu@gmail.com>
2022-11-27 14:33:06 -0500

    fixed alignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+//import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage, AmplifyS3Image } from "aws-amplify";
 import Image from "./image";
@@ -9,15 +9,15 @@ import {
   Heading,
   Text,
   TextField,
-  View,
+  View, Table, TableHead, TableRow, TableCell,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
 // import { listAlbums } from "./graphql/queries";
-import {
-  createAlbum as createAlbumMutation,
-  deleteAlbum as deleteAlbumMutation,
-} from "./graphql/mutations";
-import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
+// import {
+//   createAlbum as createAlbumMutation,
+//   deleteAlbum as deleteAlbumMutation,
+// } from "./graphql/mutations";
+// import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
 
 const App = ({ signOut }) => {
   // state = { fileUrl: ''}
@@ -55,21 +55,7 @@ const App = ({ signOut }) => {
    }).catch(err => {
     console.log(err);
    })
-  }, [image]);
-
-  const showImg = (file) => {
-    Storage.get(file).then(resp => {
-      console.log(resp);
-      setImage(resp);
-    }).catch(err => {console.log(err);})
-  }
-
-  function showText(text){
-    document.getElementById("text").innerHTML=text;
-  }
-  function hide(){
-    document.getElementById("text").innerHTML="";
-  }
+  }, []);
 
   const Icon = ({fileName}) => { 
     const [image, setImage] = useState(null);
@@ -78,71 +64,38 @@ const App = ({ signOut }) => {
          setImage(resp);
       }).catch(err => {console.log(err);})
 
-    return (<div>
-          <Image rounded thumbnail src={image} zoom={true} alt="skfjkld" width="15%" 
-          onClick={() => showImg(fileName)}
-        //  onMouseOver={() => showText('Some Text')} 
-        //  onMouseOut={() => hide()}
-          />
-          <p>{fileName}</p>
-       </div>
+    return (
+          <Image rounded thumbnail src={image} zoom={true} 
+            alt={fileName} title={fileName} width='12%' />
     );
 }
 
 
   return (
     <View className="App">
-      <Heading level={1}>Photo Albums App</Heading>
-      {/* <View as="form" margin="3rem 0" onSubmit={createAlbum}>
-      <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Album Name"
-            label="Album Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="owner"
-            placeholder="Owner"
-            label="Owner"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Album
-          </Button>
-        </Flex>
-      </View> */}
-      <Heading level={2}>Current Albums</Heading>
-      <View margin="3rem 0">
-        {files.map((file) => (
-          <Flex
-            key={file.key || file.key}
+      <Table highlightOnHover={false}>
+        <TableHead>
+          <TableRow>
+            <TableCell><Heading level={3}>My family</Heading></TableCell>
+            <TableCell align="right"><Button align='right' onClick={signOut}>Sign Out</Button></TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+      <br></br>
+            {/* <View margin="3rem 0"> */}
+            <Flex
+            key="main"
             direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            {/* <Text as="strong" fontWeight={700}>
-              {key}
-            </Text> */}
-            {/* <div style={{ width: 400, margin: '0 auto' }}>
-              <AmplifyS3Image
-                imgKey={file.key}
-              />
-            </div> */}
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            alignContent="flex-start"
+          >        {files.map((file) => (
+
             <Icon fileName={file.key} ></Icon>
-          </Flex>
+
         ))}
+                  </Flex>
       </View>
-            <Flex justifyContent="center"
-            alignItems="center"><Image src={image} width="600"/></Flex>
-            <br></br>
-            <Flex justifyContent="center"
-            alignItems="center"><Button onClick={signOut}>Sign Out</Button></Flex>
-  </View>
   );
 
 }
