bc61e15d06779283e1264dc4ff467275d70eac30
wangyutony <tonywangyu@gmail.com>
2022-11-27 23:34:10 -0500

    Flex wrap

--- src/App.js
+++ src/App.js
@@ -20,52 +20,38 @@ import {
 // import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
 
 const App = ({ signOut }) => {
-  // state = { fileUrl: ''}
-  // componentDidMount() {
-  //   Storage.get('kdfjkd.jpg')
-  //   .then(data => {
-  //     this.setState()
-  //   })
-  // }
   const [files, setFiles] = useState([]);
-  const [image, setImage] = useState(null);
-  //const [map, setMap] = useState(new Map());
- // var listOfObjects = [];
-
-  const [map, setMap ] = useState(new Map());
- // let map = new Map();
-
 
   useEffect(() => {
-
-
-  Storage.configure({
+    Storage.configure({
       customPrefix: {
           public: 'pictures/',
           protected: 'pictures/',
           private: 'pictures/'
       },
-  })
+    })
 
-
-  Storage.list('').then(files => {
-    setFiles(files);    
+    Storage.list('tony2022').then(files => {
+      setFiles(files);    
  
+    files.map((file) => (console.log(file)))
     console.log("sldkfjdsjfkjlk");
    }).catch(err => {
     console.log(err);
    })
   }, []);
 
-  const Icon = ({fileName}) => { 
+  const Icon = ({fileName, file}) => { 
     const [image, setImage] = useState(null);
+
+    console.log(fileName + '........' + file.key)
 
     Storage.get(fileName).then(resp => {
          setImage(resp);
       }).catch(err => {console.log(err);})
 
     return (
-          <Image rounded thumbnail src={image} zoom={true} 
+          <Image rounded='true' thumbnail='true' src={image} zoom={true} 
             alt={fileName} title={fileName} width='12%' />
     );
 }
@@ -82,19 +68,19 @@ const App = ({ signOut }) => {
         </TableHead>
       </Table>
       <br></br>
-            {/* <View margin="3rem 0"> */}
-            <Flex
+      {/* <View margin="3rem 0"> */}
+        <Flex
             key="main"
-            direction="row"
+            direction="flex-start"
             justifyContent="flex-start"
             alignItems="flex-start"
             alignContent="flex-start"
-          >        {files.map((file) => (
-
-            <Icon fileName={file.key} ></Icon>
-
-        ))}
-                  </Flex>
+            wrap="wrap"
+          >        
+          {files.map((file) => (
+            <Icon key={file.key} width='40' fileName={file.key} file={file} ></Icon>
+          ))}
+        </Flex>
       </View>
   );
 
